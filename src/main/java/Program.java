c70d815363feecf1ec78cddabb9816912c93a713
Thad House <thadhouse1@gmail.com>
2017-06-10 19:10:11 -0700

    Initial commit

---
+++ src/main/java/Program.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+        String osname = System.getProperty("os.name");
+        String arch = System.getProperty("os.arch");
+        System.out.print("Name: ");
+        System.out.println(osname);
+        System.out.print("Arch: ");
+        System.out.println(arch);
+    }
+}
